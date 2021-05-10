ae824d4b075f539decb8d647b60b3e7ccdd262db
Vladimri Markov <31269339+vladko92@users.noreply.github.com>
2021-05-10 13:15:27 +0300

    Add files via upload

---
+++ radians_to_degrees.py
@@ -0,0 +1,7 @@
+import math
+from math import pi
+from math import  floor
+
+rad = float(input())
+deg = rad * 180 / pi
+print(math.floor(deg))
---
+++ vacation_book_list.py
@@ -0,0 +1,7 @@
+pages = int(input())
+pages_per_hour = int(input())
+days = int(input())
+time = pages / pages_per_hour
+#result = time / days
+result = time / days
+print(result)
