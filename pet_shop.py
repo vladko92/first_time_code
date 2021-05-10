81501f72a3c6d551d0c01700fa6c3654628dd9af
Vladimri Markov <31269339+vladko92@users.noreply.github.com>
2021-05-10 16:32:58 +0300

    Add files via upload

---
+++ pet_shop.py
@@ -0,0 +1,6 @@
+dog_food = int(input())
+animal_food = int(input())
+dog_food_price = dog_food * 2.50
+animal_food_price = animal_food * 4
+needed_price = dog_food_price + animal_food_price
+print(f'{needed_price} lv.')
---
+++ project_create.py
@@ -0,0 +1,5 @@
+name = input()
+project = int(input())
+hours = 3
+hours = (hours * project)
+print(f"The architect {name} will need {hours} hours to complete {project} project/s.")
